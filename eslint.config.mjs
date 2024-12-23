21b5e2de4aefb7b1940102b502ed238e5588a1a4
Narayan Maity <narayanmaitysp1997@gmail.com>
2024-12-23 18:34:36 +0530

    express server setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,8 +13,8 @@ export default tseslint.config(
       },
     },
     rules: {
-      // 'no-console': 'error',
-      // 'dot-notation': 'error',
+      'no-console': 'error',
+      'dot-notation': 'error',
     },
   }
 );
--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,17 @@
+import app from './app';
 import { Config } from './config';
 
-console.log(Config.PORT);
+const startServer = () => {
+  const PORT = Config.PORT;
+
+  try {
+    // eslint-disable-next-line no-console
+    app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
+  } catch (err) {
+    // eslint-disable-next-line no-console
+    console.error(err);
+    process.exit(1);
+  }
+};
+
+startServer();
